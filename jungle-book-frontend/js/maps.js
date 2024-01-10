09055a0f7cc049172897e241bc04d253a3e7209d
LaurinPoelderl <laurin.poelderl@gmail.com>
2024-01-10 12:26:32 +0100

    location tracking

--- jungle-book-frontend/js/maps.js
+++ jungle-book-frontend/js/maps.js
@@ -1,33 +1,54 @@
 let latitude;
 let longitude;
 let accuracy;
-let txt = "";
 let coords;
 let iframe = "";
 
-function getLocation() {
-    iframe = document.getElementById('mapIframe');
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(succsess, error);
-    } 
-    else {
-        document.getElementById("mapIframe").innerHTML = "error";
+function getLocationAwait() {
+    return new Promise((resolve, reject) => {
+        let iframe = document.getElementById('mapIframe');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    resolve(position.coords);
+                    coords = position.coords;  
+                    latitude = coords.latitude;
+                    longitude = coords.longitude;
+                    
+                    var newURL = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2655.789794695176!2d' +
+                    longitude + '!3d' + latitude +
+                    '!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477396fb96f68367%3A0xf2b265395a736637!2sHTL%20Leonding!5e0!3m2!1sde!2sat!4v1700938059421!5m2!1sde!2sat';
+                    iframe.src = newURL;
+                },
+                error => {
+                    reject(error);
+                    document.getElementById("mapIframe").innerHTML = error.message;
+                }
+            );
+        } else {
+            reject(new Error('Geolocation is not supported.'));
+        }
+    });
+}
+
+async function getLocation() {
+    let coords = "";
+    try {
+        coords = await getLocationAwait();
+        console.log(coords);
+    } catch (error) {
+        console.error(error);
     }
-}
-function error(error) {
-    document.getElementById("mapIframe").innerHTML = error.message;
-    
+    return coords;
 }
 
-function succsess(position) {
-    coords = position.coords;  
+async function checkLocation() {
+    let coords = "";    
+    coords = await getLocation();
+    
+    accuracy = coords.accuracy;
     latitude = coords.latitude;
     longitude = coords.longitude;
-    accuracy = coords.accuracy;
-    
-    var newURL = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2655.789794695176!2d' +
-    longitude + '!3d' + latitude +
-    '!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477396fb96f68367%3A0xf2b265395a736637!2sHTL%20Leonding!5e0!3m2!1sde!2sat!4v1700938059421!5m2!1sde!2sat';
-    iframe.src = newURL; 
+    console.log(latitude, longitude, accuracy);
 }
