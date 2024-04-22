84629471e66be50a7ffa29cd8e303287d664e5b5
BitMatse <>
2024-04-22 21:05:03 +0200

    Added java docs

--- jungle-book-backend/src/main/java/at/htlleonding/junglebook/boundary/CheckpointResource.java
+++ jungle-book-backend/src/main/java/at/htlleonding/junglebook/boundary/CheckpointResource.java
@@ -13,17 +13,34 @@ public class CheckpointResource {
     @Inject
     CheckpointRepository checkpointRepository;
 
+    /**
+     * Add a new checkpoint to the database
+     *
+     * @param checkpoints a string of checkpoints in CSV format
+     */
     @POST
     @Path("/add-checkpoints")
     @Consumes(MediaType.TEXT_PLAIN)
     public void addCheckpoints(String checkpoints) {
         checkpointRepository.addCheckpoints(checkpoints);
     }
+
+    /**
+     * Remove a checkpoint from the database
+     *
+     * @param id the id of the checkpoint to remove
+     */
     @GET
     @Path("/remove-checkpoint?id={id}")
     public void removeCheckpoints(@PathParam("id") long id) {
         checkpointRepository.removeCheckpoint(id);
     }
+
+    /**
+     * Edit a checkpoint in the database
+     *
+     * @param checkpoint the edited checkpoint
+     */
     @POST
     @Path("/edit-checkpoint")
     @Consumes(MediaType.TEXT_PLAIN)
@@ -31,13 +48,24 @@ public class CheckpointResource {
         checkpointRepository.editCheckpoint(checkpoint);
     }
 
-
+    /**
+     * Get all checkpoints from the database
+     *
+     * @return a list of all checkpoints
+     */
     @GET
     @Path("/list")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Checkpoint> getAllCheckpoints() {
         return checkpointRepository.getAllCheckpoints();
     }
+
+    /**
+     * Get a checkpoint by its id
+     *
+     * @param id the id of the checkpoint
+     * @return the checkpoint with the given id
+     */
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
