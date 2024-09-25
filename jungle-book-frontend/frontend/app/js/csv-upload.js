4ed2784a1f5bee0a26c3c6656c1aed9bb55c2fc5
LaurinPoelderl <laurin.poelderl@gmail.com>
2024-09-25 20:22:50 +0200

    csv upload

---
+++ jungle-book-frontend/frontend/app/js/csv-upload.js
@@ -0,0 +1,51 @@
+document.getElementById('file-upload').addEventListener('change', function() {
+    const filePath = this.value;
+    const allowedExtensions = /(\.csv)$/i;
+
+    if (!allowedExtensions.exec(filePath)) {
+        document.getElementById('message').innerText = 'Bitte wähle eine gültige CSV-Datei.';
+        this.value = '';
+        return false;
+    } else {
+        document.getElementById('message').innerText = '';
+    }
+});
+
+document.getElementById('upload-btn').addEventListener('click', function() {
+    const fileInput = document.getElementById('file-upload');
+
+    if (fileInput.files.length === 0) {
+        document.getElementById('message').innerText = 'Bitte wähle eine CSV-Datei aus.';
+        return;
+    }
+
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+        const csvData = event.target.result;
+
+        console.log("CSV-Daten:", csvData);
+
+        fetch('http://138.2.138.238:8000/api/checkpoint/add-checkpoints', {
+            method: 'POST',
+            headers: { 'Content-Type': 'text/plain' },
+            body: csvData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Erfolgreich hochgeladen:', data);
+            document.getElementById('message').innerText = 'Datei erfolgreich hochgeladen!';
+        })
+        .catch((error) => {
+            console.error('Fehler beim Hochladen:', error);
+            document.getElementById('message').innerText = 'Fehler beim Hochladen der Datei.';
+        });
+    };
+
+    reader.onerror = function() {
+        document.getElementById('message').innerText = 'Fehler beim Lesen der Datei.';
+    };
+
+    reader.readAsText(file);
+});
