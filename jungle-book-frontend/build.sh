366957b4c5c81ba054e10ea924779c3a408e47a9
mschablinger <m.schablinger@students.htl-leonding.ac.at>
2024-09-26 10:18:56 +0200

    Fixed deployment

--- jungle-book-frontend/build.sh
+++ jungle-book-frontend/build.sh
@@ -2,5 +2,5 @@ rm -rf target
 mkdir -p target/nginx
 cp ./docker/default.conf ./target/nginx/
 mkdir -p ./target/frontend
-cp -r ./frontend ./target/frontend/
+cp -r ./frontend/* ./target/frontend/
 docker build --tag frontend --file ./docker/Dockerfile ./target/
